657480294b80e6d99d6b3e6b63ba7f530b36a798
JeffeDev <jeffe.info@gmail.com>
2022-07-27 11:02:05 -0300

    Documentaçao de Código JUnit Test com Selenium

--- pedidos-api/src/test/java/br/com/apipedidos/controller/ProdutoControllerTest.java
+++ pedidos-api/src/test/java/br/com/apipedidos/controller/ProdutoControllerTest.java
@@ -4,10 +4,21 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+/*
+ * @author: Jefferson César de Souza
+ * 			Teste Unitário Usando Selenium
+ * 
+ * @version:Usar JUnit5 e import org.junit.jupiter.api
+ * 			no JUnit 5 uma nova classe chamada Assertions, presente 
+ * 			no pacote org.junit.jupiter.api, foi criada e recomenda-se a sua 
+ * 			utilização em substituição à antiga classe Assert.
+ * 
+ * @exception: lança uma exception caso o link não estiver acessível.
+ * 			Este teste deve ser executado com o sistema em execução
+ * 
+ */
 class ProdutoControllerTest {
-	// Teste Unitário Usando Selenium:
-	// Este teste deve ser executado com o sistema em execução
-	// casocontrário o link não poderá ser acessado
+
 	@Test
 	void deveriaAbriroNavegadorEAcessaroLinkproduto() {
         System.setProperty("webdriver.chrome.driver", "driverSeleniumChrome/chromedriver.exe");
